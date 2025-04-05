fe728cd646e270488e0d67966fd3f9817a0891ef
Matthew Heick <github@heick.email>
2025-04-05 13:42:45 -0400

    png to svg, adjustable dimensions

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ Type stuff into text box, QR code is generated.
 
 # Dependencies
 - phpqrcode 1.1.4 via [https://phpqrcode.sourceforge.net/](https://sourceforge.net/projects/phpqrcode/files/releases/)
+
+# Todo
+- Saw Taco Bell use an image as a qr code (which [linked to their app](qr.tacobell.com/2p7gy-qr) ). Hopefully want to be able to take an image as input and generate the same.
--- index.php
+++ index.php
@@ -5,20 +5,30 @@ require_once('./phpqrcode/phpqrcode.php');
  */
 if (isset($_POST['qr']))
 {
-	$qrtext = $_POST['qr'];
-	$qrtemp = tempnam(sys_get_temp_dir(), 'qr-');
-	if (substr($qrtemp, -4) !== '.png')
-	{
-		if (file_exists($qrtemp))
-		{
-			unlink($qrtemp);
-		}
-		$qrtemp .= '.png';
+	$qrtext = $_POST['qr']; /* not trimming because spaces might be necessary */
+	if (strlen($qrtext) == 0) {
+		echo '{"error": "no data"}';
+		die();
 	}
-	QRcode::png($qrtext, $qrtemp);
-	$qrdata = base64_encode(file_get_contents($qrtemp));
-	unlink($qrtemp);
-	$return = ['image' => 'data:image/png;base64,' . $qrdata, 'text' => $qrtext];
+	$qr_data = QRcode::text($qrtext);
+	$svg_dimensions = count($qr_data);
+	$svg_data = [];
+	$svg_data[] = '<svg viewBox="0 0 ' . $svg_dimensions . ' ' . $svg_dimensions . '" xmlns="http://www.w3.org/2000/svg">';
+	for ($y = 0; $y < $svg_dimensions; $y++)
+	{
+		$horizon = $qr_data[$y];
+		for ($x = 0; $x < strlen($horizon); $x++)
+		{
+			if (substr($horizon, $x, 1) == 1)
+			{
+				$svg_data[] = ' <rect width="1" height="1" x="' . $x . '" y="' . $y . '" style="fill:black;stroke:black;stroke-width:0;" />';
+			}
+		}
+	}
+	$svg_data[] = '</svg>';
+	$svg = implode("\n", $svg_data);
+	$qrdata = base64_encode($svg);
+	$return = ['image' => 'data:image/svg+xml;base64,' . $qrdata, 'text' => $qrtext];
 	$json = json_encode($return);
 	if ($json === false)
 	{
@@ -52,7 +62,13 @@ function generateQR() {
 			}
 			if ("image" in j)
 			{
-				document.getElementById('qr-image').src = j.image;
+				let dim = document.getElementById('qr-dimensions');
+				let qr = document.getElementById('qr-image');
+				qr.src = j.image;
+				let px = parseInt(dim.value);
+				if (px < 30) { px = 30; }
+				qr.style.width = px + 'px';
+				qr.style.height = px + 'px';
 			}
 		}
 	};
@@ -67,7 +83,10 @@ function generateQR() {
 			Enter Data to turn to QR code:
 		</div>
 		<div>
-			<textarea id="make-into-qr">enter text here</textarea>
+			<textarea id="make-into-qr">Enter stuff here</textarea>
+		</div>
+		<div>
+			Image px:<input id="qr-dimensions" type="text" value="50" placeholder="50" />
 		</div>
 		<div>
 			<img id="qr-image" src="" title="qr code" />
@@ -78,6 +97,10 @@ window.addEventListener("load", (event) => {
 	let qr = document.getElementById('make-into-qr');
 	qr.addEventListener('keyup', generateQR);
 	qr.focus();
+});
+window.addEventListener("load", (event) => {
+	let resize = document.getElementById('qr-dimensions');
+	resize.addEventListener('keyup', generateQR);
 });
 	</script>
 </html>
